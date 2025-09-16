4bc744697aca091de7a9729e6218b8725039d918
emilly347 <emygomes.roseno@gmail.com>
2025-09-16 11:48:12 +0000

    Emilly Gomes Roseno Da Silva Modulo I Turma manhã

--- Avaliação 1/avaliacao.py
+++ Avaliação 1/avaliacao.py
@@ -1,4 +1,28 @@
 # CALCULADORA DE IMC COM CATEGORIZAÇÃO
-# ENTREGA ATÉ 04/09/2025
-
 # FAZER UM FORK E UM PULL REQUEST PARA ENTREGAR
+
+peso = float(input("Digite o seu peso: "))
+altura = float(input("Digite a altura: "))
+
+imc = peso / (altura**2)
+
+print(imc)
+
+if imc <= 18.5:
+    print("abaixo do peso.")
+
+elif imc <= 24.9:
+    print("Peso noramal. ")
+
+elif imc <= 29.9:
+    print("Sobre peso. ")
+
+elif imc <= 34.9:
+    print("Obesidade Grau 1. ")
+
+elif imc <= 39.9: 
+    print("Obesidade Grau 2.") 
+
+else:
+    print("Obesidade grau 3. ")
+
